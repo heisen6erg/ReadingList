11a91b791d3fa429b096b3361e27ec1a88c8acc2
heisen6erg <andryus.omelchenko@gmail.com>
2014-09-24 12:35:16 +0300

    Add books.

---
+++ db/migrate/20140923123239_finished_on_to_books.rb
@@ -0,0 +1,4 @@
+class FinishedOnToBooks < ActiveRecord::Migration
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Book.destroy_all
+
+Book.create!([{
+	title: "HTML and CSS: Design and Build Websites",
+	author: "Jon Duckett",
+	description: "A full-color introduction to the basics of HTML and CSS from the publishers of Wrox! Every day, more and more people want to learn some HTML and CSS. Joining the professional web designers and programmers are new audiences who need to know a little bit of code at work (update a content management system or e-commerce store) and those who want to make their personal blogs more attractive. Many books teaching HTML and CSS are dry and only written for those who want to become programmers, which is why this book takes an entirely new approach. Introduces HTML and CSS in a way that makes them accessible to everyone—hobbyists, students, and professionals—and it’s full-color throughout Utilizes information graphics and lifestyle photography to explain the topics in a simple way that is engaging Boasts a unique structure that allows you to progress through the chapters from beginning to end or just dip into topics of particular interest at your leisure This educational book is one that you will enjoy picking up, reading, then referring back to. It will make you wish other technical topics were presented in such a simple, attractive and engaging way! This book is also available as part of a set in hardcover - Web Design with HTML, CSS, JavaScript and jQuery, 9781119038634; and in softcover - Web Design with HTML, CSS, JavaScript and jQuery, 9781118907443.",
+	amazon_id: "1118008189",
+	rating: 4,
+	finished_on: 10.days.ago
+	},
+	{
+	title: "JavaScript and JQuery: Interactive Front-End Web Development",
+	author: "Jon Duckett",
+	description: "THIS BOOK COVERS:
+		Basic programming concepts - assuming no prior knowledge of programming beyond an ability to create a web page using HTML & CSS
+		Core elements of the JavaScript language - so you can learn how to write your own scripts from scratch
+		jQuery - which will allow you to simplify the process of writing scripts (this is introduced half-way through the book once you have a solid understanding of JavaScript)
+		How to recreate techniques you will have seen on other web sites such as sliders, content filters, form validation, updating content using Ajax, and much more (these examples demonstrate writing your own scripts from scratch and how the theory you have learned is put into practice).
+		As with our first book (the best-selling HTML & CSS: Design and Build Websites), each chapter:
+		Breaks subjects down into bite-sized chunks with a new topic on each page
+		Contains clear descriptions of syntax, each one demonstrated with inspiring code samples
+		Uses diagrams and photography to explain complex concepts in a visual way
+		By the end of the book, not only will you be able to use the thousands of scripts, JavaScript APIs, and jQuery plugins that are freely available on the web, and be able to customize them - you will also be able to create your own scripts from scratch.
+
+		If you're looking to create more enriching web experiences, then this is the book for you.",
+	amazon_id: "1118531647",
+	rating: 5,
+	finished_on: 4.days.ago 		
+	},
+	{
+	title: "Windows 8.1 For Dummies Book ",
+	author: "Andy Rathbone",
+	description: "A smart value — the complete book plus three hours of video training!
+
+Windows 8.1 brings some welcome tweaks to the Windows operating system, including more ways to customize the interface and boot operations, a new start button that reveals apps, greater integration between the two interfaces, and app updates. This bargain bundle includes the complete Windows 8.1 For Dummies book plus a DVD that walks you visually through the most common Windows tasks. Whether you're just getting started with Windows or are upgrading to 8.1, you can learn everything you need to know, see how things are done, and start using Windows 8.1 with confidence.
+
+Includes the complete Windows 8.1 For Dummies plus three hours of video training on DVD
+Covers all the upgrades that make up Window 8.1, including new opportunities to customize the interface, the new start button, and greater integration between the two interface options
+Features complete information on using a Windows computer for those new to the system
+The DVD provides basic overviews and step-by-step instruction covering the essentials of Windows 8.1, such as getting started, using the start screen and the desktop, getting things done online, managing photos, playing music, customizing Windows, and much more
+Windows 8.1 For Dummies Book + DVD Bundle is a value-priced crash course for anyone wanting to understand and use Windows 8.1.",
+	amazon_id: "1118821076",
+	rating: 2,
+	finished_on: nil
+		}])
